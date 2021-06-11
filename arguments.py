dff2cdfbce81356d89ca3fde405be13d4171adac
bonniehyeon <50580028+bonniehyeon@users.noreply.github.com>
2021-06-11 14:43:24 +0900

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -7,7 +7,7 @@ class ModelArguments:
     Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
     """
     model_name_or_path: str = field(
-        default="bert-base-multilingual-cased",
+        default="monologg/koelectra-base-v3-discriminator",
         metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
     )
     config_name: Optional[str] = field(
